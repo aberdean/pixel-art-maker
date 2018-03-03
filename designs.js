ab0ccfcd5ccb478e748d6eea7cee0d98925c4af4
Antonella Bernobich Dean <aberdean@users.noreply.github.com>
2018-03-03 07:06:34 +0100

    Fix style issues

--- designs.js
+++ designs.js
@@ -1,48 +1,53 @@
 // Select color input and set color
 let color = "#000";
 const setColor = document.getElementById("color-picker");
+
 setColor.addEventListener("change", function() {
-	color = this.value;
+    color = this.value;
 });
 
 // Select size input and set height and width
 const setHeight = document.getElementById("input-height");
 let height = setHeight.value;
+
 setHeight.addEventListener("change", function() {
-	height = this.value;
+    height = this.value;
 });
 
 const setWidth = document.getElementById("input-weight");
 let width = setWidth.value;
-setWidth.addEventListener("change", function () {
-	width = this.value;
+
+setWidth.addEventListener("change", function() {
+    width = this.value;
 });
 
 // Select table
 const table = document.getElementById("pixel-canvas");
 
-// When size is submitted by the user, clear the previous grid and 
+// When size is submitted by the user, clear the previous grid and
 // build the new grid.
 document.getElementById("size-picker")
-	.addEventListener("submit", function(event) {
-		event.preventDefault();
-		clearGrid();
-		makeGrid();
-});
+        .addEventListener("submit", function(event) {
+            event.preventDefault();
+            clearGrid();
+            makeGrid();
+        });
 
 /**
  * Given a width and a height, builds a grid of those dimensions.
- * When a cell of the grid is clicked, changes the color to the 
+ * When a cell of the grid is clicked, changes the color to the
  * set color.
  */
 function makeGrid() {
-	
-	for (let i = 1; i <= height; i++) {
-		const tr = document.createElement("tr");
-		console.log(height);
-		table.appendChild(tr);
-		for (let j = 1; j <= width; j++) {
-			const td = document.createElement("td");
+
+    for (let i = 1; i <= height; i++) {
+        const tr = document.createElement("tr");
+
+        table.appendChild(tr);
+
+        for (let j = 1; j <= width; j++) {
+            const td = document.createElement("td");
+
 			tr.appendChild(td);
 		}
 	}
